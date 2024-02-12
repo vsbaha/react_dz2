2632bed8bc5f69f96fe8f9dc9abadcc5787f9377
vsbaha <azimkanovbaktiar@gmail.com>
2024-02-12 20:04:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Создайте файл App.css для стилей
 
 function App() {
+  const [redFirst, setRedFirst] = useState(true);
+
+  const handleClick = () => {
+    setRedFirst(!redFirst);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {redFirst ? (
+        <>
+          <div className="red-block" onClick={handleClick}></div>
+          <div className="blue-block" onClick={handleClick}></div>
+        </>
+      ) : (
+        <>
+          <div className="blue-block" onClick={handleClick}></div>
+          <div className="red-block" onClick={handleClick}></div>
+        </>
+      )}
     </div>
   );
 }
